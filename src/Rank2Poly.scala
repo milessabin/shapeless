4f8f701347014da2e91476274b7c8555651a04c6
Miles Sabin <miles@milessabin.com>
2011-10-20 16:49:48 +0100

    Fixed ~>'s with constant result types; initial support for 2-arg fns.

--- src/Rank2Poly.scala
+++ src/Rank2Poly.scala
@@ -10,7 +10,17 @@ object Rank2Poly {
   }
 
   // Use of dependent type h.λ[T] essential here
-  implicit def univInst[F[_], G[_], T](h : F ~> G) : h.λ[T] = h.apply _
+  implicit def univInst1[F[_], G[_], T](h : F ~> G) : h.λ[T] = h.apply _
+  implicit def univInst2[F[_], Out, T](h : F ~> Const[Out]#λ) : h.λ[T] = h.apply _
+
+  trait HFn2[F1[_], F2[_], G[_]] {
+    type λ[T] = (F1[T], F2[T]) => G[T]
+    def apply[T](x : F1[T], y : F2[T]) : G[T]
+  }
+  
+  // Use of dependent type h.λ[T] essential here
+  implicit def univInst21[F1[_], F2[_], G[_], T](h : HFn2[F1, F2, G]) : h.λ[T] = h.apply _
+  implicit def univInst22[F1[_], F2[_], Out, T](h : HFn2[F1, F2, Const[Out]#λ]) : h.λ[T] = h.apply _
 
   object identity extends (Id ~> Id) {
     def apply[T](t : T) = t
@@ -33,7 +43,7 @@ object Rank2Poly {
   }
   
   object isDefined extends (Option ~> Const[Boolean]#λ) {
-    def apply[T](o : Option[T]) = o.isDefined
+    def apply[T](o : Option[T]) : Boolean = o.isDefined
   }
   
   object get extends (Option ~> Id) {
@@ -44,6 +54,14 @@ object Rank2Poly {
     def apply[T](t : T) : Option[T] = Option(t)
   }
   
+  object toInt extends (Id ~> Const[Int]#λ) {
+    def apply[T](t : T) : Int = t.toString.toInt
+  }
+  
+  object sum extends HFn2[Id, Id, Const[Int]#λ] {
+    def apply[T](x : T, y : T) : Int = toInt(x)+toInt(y)
+  }
+  
   def main(args : Array[String]) {
     
     // Direct application
@@ -51,6 +69,9 @@ object Rank2Poly {
     println(s1)
     val s2 = singleton("foo")
     println(s2)
+    
+    def app[G[_]](f : Int => G[Int]) = f(23)
+    app(singleton)
     
     // Implicit conversion to monomorphic function values
     val l1 = List(1, 2, 3) map singleton
@@ -74,5 +95,22 @@ object Rank2Poly {
     println(m1)
     val m2 = pairMap(list)
     println(m2)
+    
+    val l4 = List(1, 2, 3)
+    val sm1 = l4.foldLeft(0)(sum)
+    println(sm1)
+    
+    println(sum(23, 45))
+    println(sum("2", "5"))
+    
+    def combine(f : (Int, Int) => Int) = f(2, 3)
+    println(combine(sum))
+    
+    val l5 = List(1, 2, 3)
+    val l6 = l5 map option
+    println(l6)
+    
+    val l7 = l6 map isDefined
+    println(l7)
   }
 }
