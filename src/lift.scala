9e0e8d68cb419c45b66db28a71b1b78351e6fc49
Miles Sabin <miles@milessabin.com>
2011-10-30 11:58:02 +0000

    First working liftO.

--- src/lift.scala
+++ src/lift.scala
@@ -4,7 +4,7 @@ object LiftOFn {
   import Tuples._
   import Functions._
 
-  def liftO[In <: HList, Out <: HList, R](f :  In => R)(implicit m : Mapper[Option, Id, Out, In], lf : LeftFolder[Out, Boolean, Option]) : Out => Option[R] = 
+  def liftO[In <: HList, Out <: HList, R](f :  In => R)(implicit maux : Mapper[Id, Option, In, Out], m : Mapper[Option, Id, Out, In], lf : LeftFolder[Out, Boolean, Option]) : Out => Option[R] = 
     (o : Out) => if(lf(o, true, isDefined, _ && _)) Some(f(m(get, o))) else None 
 }
 
@@ -47,28 +47,26 @@ object TestLiftOFn {
     
     import Functions.Implicits._
     
-    val lf = implicitly[LeftFolder[Option[Int] :: Option[Int] :: HNil, Boolean, Option]]
-    val m = implicitly[Mapper[Option, Id, Option[Int] :: Option[Int] :: HNil, Int :: Int :: HNil]]
+    val sumO = liftO(sum)
+
+    val s2 = sumO(Some(1), Some(2))
+    println(s2)
+
+    val s3 = sumO(Some(1), None)
+    println(s3)
     
-//    val sumO = liftO(sum) //(lo)
-//    val prdO = liftO(prd)
-//
-//    val s2 = sumO(Some(1), Some(2))
-//    println(s2)
-//
-//    val s3 = sumO(Some(1), None)
-//    println(s3)
-//    
-//    val s4 = sumO(None, Some(2))
-//    println(s4)
-//    
-//    val s5 = sumO(None, None)
-//    println(s5)
-//    
-//    val p2 = prdO(Some(2), Some(3), Some(4))
-//    println(p2)
-//
-//    val p3 = prdO(Some(2), None, Some(4))
-//    println(p3)
+    val s4 = sumO(None, Some(2))
+    println(s4)
+    
+    val s5 = sumO(None, None)
+    println(s5)
+    
+    val prdO = liftO(prd)
+
+    val p2 = prdO(Some(2), Some(3), Some(4))
+    println(p2)
+
+    val p3 = prdO(Some(2), None, Some(4))
+    println(p3)
   }
 }
