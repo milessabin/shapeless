1865a1c1175dc6a9e279db5384a75e0819d9c510
Miles Sabin <miles@milessabin.com>
2011-11-10 16:57:55 +0000

    Added infrastructure to support simple natural transformations; more examples.

--- src/polyfuncases.scala
+++ src/polyfuncases.scala
@@ -18,9 +18,17 @@ trait LowPriorityPolyFunCases {
   trait ~>[F[_], G[_]] {
     type λ[T] = (F[T] => G[T]) @@ this.type
     def λ[T](c : F[T] => G[T]) = tag[this.type](c)
+    def taggedDflt[T] = λ[T](dflt)
     
-    def apply[F, G](f : F)(implicit c : (F => G) @@ this.type) : G = c(f)
+    def apply[T](f : F[T])(implicit c : (F[T] => G[T]) @@ this.type = taggedDflt[T]) : G[T] = c(f)
+    def dflt[T](f : F[T]) : G[T]
   }
+
+  implicit def univInst1[F[_], G[_], T](h : F ~> G)
+    (implicit c : (F[T] => G[T]) @@ h.type = h.taggedDflt[T]) : h.λ[T] = c
+    
+  implicit def univInst2[F[_], G, T](h : F ~> Const[G]#λ)
+    (implicit c : (F[T] => G) @@ h.type = h.taggedDflt[T]) : h.λ[T] = c
 }
 
 object PolyFunCases extends LowPriorityPolyFunCases {
@@ -30,7 +38,45 @@ object TestPolyFunCases {
   import HList._
   import PolyFunCases._
   
-  object size extends (Id ~> Const[Int]#λ)
+  object identity extends (Id ~> Id) {
+    def dflt[T](t : T) = t
+  }
+
+  object singleton extends (Id ~> Set) {
+    def dflt[T](t : T) = Set(t)
+  }
+
+  object choose extends (Set ~> Option) {
+    def dflt[T](s : Set[T]) = s.headOption
+  }
+
+  object list extends (Id ~> List) {
+    def dflt[T](t : T) = List(t)
+  }
+  
+  object headOption extends (List ~> Option) {
+    def dflt[T](t : List[T]) = t.headOption
+  }
+  
+  object isDefined extends (Option ~> Const[Boolean]#λ) {
+    def dflt[T](o : Option[T]) : Boolean = o.isDefined
+  }
+  
+  object get extends (Option ~> Id) {
+    def dflt[T](o : Option[T]) : T = o.get
+  }
+  
+  object option extends (Id ~> Option) {
+    def dflt[T](t : T) : Option[T] = Option(t)
+  }
+  
+  object toInt extends (Id ~> Const[Int]#λ) {
+    def dflt[T](t : T) : Int = t.toString.toInt
+  }
+  
+  object size extends (Id ~> Const[Int]#λ) {
+    def dflt[T](t : T) = 1
+  }
 
   implicit def sizeInt = size.λ[Int](x => 1)
   implicit def sizeString = size.λ[String](s => s.length)
@@ -53,5 +99,59 @@ object TestPolyFunCases {
 
     val st = size((23, "foo"))
     println(st)
+    
+    val ls = List("foo", "bar", "baz")
+    val lss = ls map size
+    val lsi = ls map identity
+    
+    val is = identity("foo")
+
+    // Direct application
+    val s1 = singleton(23)
+    println(s1)
+    val s2 = singleton("foo")
+    println(s2)
+    
+    def app[G[_]](f : Int => G[Int]) = f(23)
+    val as = app(singleton)
+    val al = app(list)
+    
+    // Implicit conversion to monomorphic function values
+    val l1 = List(1, 2, 3) map singleton
+    println(l1)
+    val l2 = List("foo", "bar", "baz") map list
+    println(l2)
+    val l3 = List(List(1), List(2), List(4)) map headOption
+    println(l3)
+
+    // Use as polymorphic function values
+    def pairApply[G[_]](f : Id ~> G) = (f(23), f("foo"))
+
+    val a1 = pairApply(singleton)
+    println(a1)
+    val a2 = pairApply(list)
+    println(a2)
+    val a3 = pairApply[Const[Int]#λ](size)
+    println(a3)
+    
+    // Use as polymorphic function values with type-specific cases
+    def pairApply2[G[_]](f : Id ~> G)(implicit fi : f.λ[Int], fs : f.λ[String]) = (f(23), f("foo"))
+    
+    val a4 = pairApply2[Const[Int]#λ](size)
+    println(a4)
+
+    def pairMap[F[_]](f : Id ~> F) = (List(1, 2, 3) map f, List("foo", "bar", "baz") map f)
+  
+    val m1 = pairMap(singleton)
+    println(m1)
+    val m2 = pairMap(list)
+    println(m2)
+    
+    val l5 = List(1, 2, 3)
+    val l6 = l5 map option
+    println(l6)
+    
+    val l7 = l6 map isDefined
+    println(l7)
   }
 }
