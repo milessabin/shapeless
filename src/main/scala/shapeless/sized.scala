bb8430bfa97e1a5f2add5b39eca243009671cf87
Miles Sabin <miles@milessabin.com>
2012-01-20 12:33:00 +0000

    Made Sized instance creation DRYer.

--- src/main/scala/shapeless/sized.scala
+++ src/main/scala/shapeless/sized.scala
@@ -17,70 +17,47 @@ abstract class Sized[Repr, L <: Nat](r : Repr) { outer =>
   
   def head(implicit ev : _0 < L) : A = r.head
   
-  def tail[M <: Nat](implicit pred : Pred[L, M]) = new Sized[Repr, M](r.tail) {
-    type A = outer.A
-    implicit val conv = outer.conv
-  }
+  def tail[M <: Nat](implicit pred : Pred[L, M]) = Sized[M](r.tail)
   
-  def take[M <: Nat](implicit diff : DiffAux[L, M], ev : ToInt[M]) = new Sized[Repr, M](r.take(toInt[M])) {
-    type A = outer.A
-    implicit val conv = outer.conv
-  }
+  def take[M <: Nat](implicit diff : DiffAux[L, M], ev : ToInt[M]) = Sized[M](r.take(toInt[M]))
   
-  def drop[M <: Nat](implicit diff : DiffAux[L, M], ev : ToInt[M]) = new Sized[Repr, diff.Out](r.drop(toInt[M])) {
-    type A = outer.A
-    implicit val conv = outer.conv
-  }
+  def drop[M <: Nat](implicit diff : DiffAux[L, M], ev : ToInt[M]) = Sized[diff.Out](r.drop(toInt[M]))
   
-  def splitAt[M <: Nat](implicit diff : DiffAux[L, M], ev : ToInt[M]) = {
-    val prefix = new Sized[Repr, M](r.take(toInt[M])) {
-      type A = outer.A
-      implicit val conv = outer.conv
-    }
-    val suffix = new Sized[Repr, diff.Out](r.drop(toInt[M])) {
-      type A = outer.A
-      implicit val conv = outer.conv
-    }
-    
-    (prefix, suffix)
-  }
+  def splitAt[M <: Nat](implicit diff : DiffAux[L, M], ev : ToInt[M]) = (take[M], drop[M])
   
   def +:(elem : A)(implicit cbf : CanBuildFrom[Repr, A, Repr]) : Sized[Repr, Succ[L]] = {
     val builder = cbf.apply(r)
     builder += elem
     builder ++= r.toIterator
-    new Sized[Repr, Succ[L]](builder.result) {
-      type A = outer.A
-      implicit val conv = outer.conv
-    }
+    Sized[Succ[L]](builder.result)
   }
   
   def ++[B >: A, That <% GenTraversableLike[B, That], M <: Nat, N <: Nat](that : Sized[That, M] { type A = B })
     (implicit
       ev : Sum[L, M, N],
       cbf : CanBuildFrom[Repr, B, That]
-    ) = new Sized[That, N](r ++ that.unsized) {
-    type A = that.A
-    implicit val conv = that.conv
-  }
+    ) = Sized[N](r ++ that.unsized)
 }
 
 class SizedOps[A0, Repr <% GenTraversableLike[A0, Repr]](r : Repr) {
   import Nat._
-  def sized[L <: Nat](implicit ev : ToInt[L]) : Option[Sized[Repr, L] { type A = A0 }] = {
-    if(r.size == toInt[L]) {
-      val conv0 = implicitly[Repr => GenTraversableLike[A0, Repr]]
-      val nl = new Sized[Repr, L](r) {
-        type A = A0
-        implicit val conv = conv0
-      }
-      Some(nl)
-    }
-    else None
-  }
+  def sized[L <: Nat](implicit ev : ToInt[L]) : Option[Sized[Repr, L] { type A = A0 }] =
+    if(r.size == toInt[L]) Some(Sized[L](r)) else None
 }
 
 object Sized {
+  class SizedBuilder[L <: Nat] {
+    def apply[A0, Repr <% GenTraversableLike[A0, Repr]](r : Repr) = { 
+      val conv0 = implicitly[Repr => GenTraversableLike[A0, Repr]]
+      new Sized[Repr, L](r) {
+        type A = A0
+        implicit val conv = conv0
+      }
+    }
+  }
+  
+  def apply[L <: Nat] = new SizedBuilder[L]
+  
   implicit def genTraversableSizedOps[CC[X] <: GenTraversable[X], T](cc : CC[T])
     (implicit conv : CC[T] => GenTraversableLike[T, CC[T]]) = new SizedOps[T, CC[T]](cc)
   
