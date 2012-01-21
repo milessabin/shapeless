10b5fb48d41094158426b3f626ade9ebfa5f1919
Miles Sabin <miles@milessabin.com>
2012-01-21 03:06:12 +0000

    First cut at making Sized covariant in it's representation type.

--- examples/src/main/scala/shapeless/examples/sized.scala
+++ examples/src/main/scala/shapeless/examples/sized.scala
@@ -8,48 +8,54 @@ object SizedExample1 extends App {
   
   def sequence[T](lo : List[Option[T]]) = if (lo.exists(_ isEmpty)) None else Some(lo.map(_.get))
   
+  def row(cols : Seq[String]) = cols.mkString("\"", "\", \"", "\"")
+  
+  def csv[N <: Nat](hdrs : Sized[Seq[String], N], rows : List[Sized[Seq[String], N]]) =
+    row(hdrs) :: rows.map(row(_))
+  
   val hdrs = List("Title", "Author")
   val rows = List(List("foo", "Mr X"), List("bar", "Mr Y"))
   
-  def row(cols : List[String]) = cols.mkString("\"", "\", \"", "\"")
-  
-  def csv[N <: Nat](hdrs : Sized[List[String], N], rows : List[Sized[List[String], N]]) =
-    row(hdrs) :: (rows map (r => row(r)))
-  
-  val sizedHdrs = hdrs.sized[_2]
-  val sizedRows = sequence(rows map (_.sized[_2]))
-    
-  val formatted = 
-    for {
-      h  <- sizedHdrs 
-      rs <- sizedRows
-    } yield csv(h, rs)
-    
-  formatted map (_ foreach println)
+  val threeHdrs = List("Title", "Author", "ISBN")
+
+  for {
+    shdrs <- hdrs.sized[_2] 
+    srows <- sequence(rows map (_.sized[_2]))
+  } {
+    val formatted = csv(shdrs, srows)
+    formatted foreach println
+  }
+
+  for {
+    shdrs <- threeHdrs.sized[_2] 
+    srows <- sequence(rows map (_.sized[_2]))
+  } {
+    // Not reached
+    val formatted = csv(shdrs, srows)
+    formatted foreach println
+  }
 }
 
-object SizedExample2 {
+object SizedExample2 extends App {
   import shapeless.Nat
   import shapeless.Nat._
   import shapeless.Sized
   import shapeless.Sized._
 
-  val hdrs = Sized[Seq]("Title", "Author")  // Sized[List[Int], _2]
-  val rows = List(                          // List[Sized[List[Int], _2]]
-    Sized[Seq]("foo", "Mr X"),
-    Sized[Seq]("bar", "Mr Y")
-  )
-
   def row(cols : Seq[String]) = cols.mkString("\"", "\", \"", "\"")
   
   def csv[N <: Nat](hdrs : Sized[Seq[String], N], rows : List[Sized[Seq[String], N]]) =
-    row(hdrs) :: (rows map (r => row(r)))
+    row(hdrs) :: rows.map(row(_))
     
+  val hdrs = Sized("Title", "Author")  // Sized[List[String], _2]
+  val rows = List(                     // List[Sized[List[String], _2]]
+    Sized("foo", "Mr X"),
+    Sized("bar", "Mr Y")
+  )
+
   val formatted = csv(hdrs, rows)
   formatted foreach println
   
   val threeHdrs = Sized[Seq]("Title", "Author", "ISBN") // Sized[List[Int], _3]
   //val badFormatted = csv(threeHdrs, rows) // Does not compile
-  
-  val f : (Int*) => List[Int] = List.apply
 }
--- src/main/scala/shapeless/sized.scala
+++ src/main/scala/shapeless/sized.scala
@@ -1,27 +1,30 @@
 package shapeless
 
-import scala.collection.{ GenTraversable, GenTraversableOnce, GenTraversableLike }
+import scala.collection.{ GenTraversable, GenTraversableLike }
 import scala.collection.generic.CanBuildFrom
-import scala.collection.immutable.WrappedString
 
-abstract class Sized[Repr, L <: Nat](r : Repr) { outer =>
+abstract class Sized[+Repr, L <: Nat](r : Repr) {
+  type A
+  
+  def unsized = r
+
+  override def toString = r.toString
+}
+
+class SizedOps[A, Repr <% GenTraversableLike[A, Repr], L <: Nat](r : Repr) { outer =>
   import Sized._
   import Nat._
   import LT._
   
-  type A
-  implicit val conv : Repr => GenTraversableLike[A, Repr]  
-  
-  override def toString = r.toString
-  def unsized = r
+  type A0 = A
   
   def head(implicit ev : _0 < L) : A = r.head
   
-  def tail(implicit pred : Pred[L]) = wrap[pred.Out](r.tail)
+  def tail(implicit pred : Pred[L]) = new Sized[Repr, pred.Out](r.tail) { type A = A0 }
   
-  def take[M <: Nat](implicit diff : Diff[L, M], ev : ToInt[M]) = wrap[M](r.take(toInt[M]))
+  def take[M <: Nat](implicit diff : Diff[L, M], ev : ToInt[M]) = new Sized[Repr, M](r.take(toInt[M])) { type A = A0 }
   
-  def drop[M <: Nat](implicit diff : Diff[L, M], ev : ToInt[M]) = wrap[diff.Out](r.drop(toInt[M]))
+  def drop[M <: Nat](implicit diff : Diff[L, M], ev : ToInt[M]) = new Sized[Repr, diff.Out](r.drop(toInt[M])) { type A = A0 }
   
   def splitAt[M <: Nat](implicit diff : Diff[L, M], ev : ToInt[M]) = (take[M], drop[M])
   
@@ -29,75 +32,52 @@ abstract class Sized[Repr, L <: Nat](r : Repr) { outer =>
     val builder = cbf.apply(r)
     builder += elem
     builder ++= r.toIterator
-    wrap[Succ[L]](builder.result)
+    new Sized[Repr, Succ[L]](builder.result) { type A = A0 }
   }
   
-  def ++[B >: A, That, M <: Nat](that : SizedAux[B, That, M])
+  def ++[B >: A, That, M <: Nat](that : Sized[That, M] { type A = B })
     (implicit
       sum : Sum[L, M],
-      cbf : CanBuildFrom[Repr, B, That]
-    ) = {
-    implicit val convThat = that.conv
-    wrap[sum.Out](r ++ that.unsized)
-  }
+      cbf : CanBuildFrom[Repr, B, That],
+      convThat : That => GenTraversableLike[B, That]) = new Sized[That, sum.Out](r ++ that.unsized) { type A = B }
     
   def map[B, That](f : A => B)
-    (implicit
-      cbf : CanBuildFrom[Repr, B, That],
-      convThat : That => GenTraversableLike[B, That]) = wrap[L](r map f)
+    (implicit cbf : CanBuildFrom[Repr, B, That]) = new Sized[That, L](r map f) { type A = B }
 }
 
-object Sized {
+trait LowPrioritySized {
+  implicit def sizedToRepr[Repr](s : Sized[Repr, _]) : Repr = s.unsized
+}
+
+object Sized extends LowPrioritySized {
   import Nat._
   
+  implicit def sizedOps[A0, Repr <% GenTraversableLike[A0, Repr], L <: Nat]
+    (s : Sized[Repr, L] { type A = A0 }) : SizedOps[A0, Repr, L] = new SizedOps[A0, Repr, L](s.unsized)
+  
   class SizedBuilder[CC[_]] {
-    def apply[T](a : T)
-      (implicit
-        cbf : CanBuildFrom[Nothing, T, CC[T]],
-        conv : CC[T] => GenTraversableLike[T, CC[T]]) = wrap[_1]((cbf() += a).result)
+    def apply[T](a : T)(implicit cbf : CanBuildFrom[Nothing, T, CC[T]]) = new Sized[CC[T], _1]((cbf() += a).result) { type A = T }
 
     def apply[T](a : T, b : T)
-      (implicit
-        cbf : CanBuildFrom[Nothing, T, CC[T]],
-        conv : CC[T] => GenTraversableLike[T, CC[T]]) = wrap[_2]((cbf() += (a, b)).result)
+      (implicit cbf : CanBuildFrom[Nothing, T, CC[T]]) = new Sized[CC[T], _2]((cbf() += (a, b)).result) { type A = T }
         
     def apply[T](a : T, b : T, c : T)
-      (implicit
-        cbf : CanBuildFrom[Nothing, T, CC[T]],
-        conv : CC[T] => GenTraversableLike[T, CC[T]]) = wrap[_3]((cbf() += (a, b, c)).result)
+      (implicit cbf : CanBuildFrom[Nothing, T, CC[T]]) = new Sized[CC[T], _3]((cbf() += (a, b, c)).result) { type A = T }
   }
   
   def apply[CC[_]] = new SizedBuilder[CC]
   
   def apply[CC[_]]()
-    (implicit
-      cbf : CanBuildFrom[Nothing, Nothing, CC[Nothing]],
-      conv : CC[Nothing] => GenTraversableLike[Nothing, CC[Nothing]]) = wrap[_0](cbf().result)
+    (implicit cbf : CanBuildFrom[Nothing, Nothing, CC[Nothing]]) =
+      new Sized[CC[Nothing], _0](cbf().result) { type A = Nothing }
 
-  type SizedAux[A0, Repr, L <: Nat] = Sized[Repr, L] {
-    type A = A0
-  }
-  
-  class SizedWrapper[L <: Nat] {
-    def apply[A0, Repr <% GenTraversableLike[A0, Repr]](r : Repr) = { 
-      val conv0 = implicitly[Repr => GenTraversableLike[A0, Repr]]
-      new Sized[Repr, L](r) {
-        type A = A0
-        implicit val conv = conv0
-      }
-    }
-  }
-  
-  private def wrap[L <: Nat] = new SizedWrapper[L]
-  
-  class SizedOps[A, Repr <% GenTraversableLike[A, Repr]](r : Repr) {
-    def sized[L <: Nat](implicit toInt : ToInt[L]) = if(r.size == toInt()) Some(wrap[L](r)) else None
+  class SizedConv[A0, Repr <% GenTraversableLike[A0, Repr]](r : Repr) {
+    def sized[L <: Nat](implicit toInt : ToInt[L]) =
+      if(r.size == toInt()) Some(new Sized[Repr, L](r) { type A = A0 }) else None
   }
 
   implicit def genTraversableSizedOps[CC[X] <: GenTraversable[X], T](cc : CC[T])
-    (implicit conv : CC[T] => GenTraversableLike[T, CC[T]]) = new SizedOps[T, CC[T]](cc)
+    (implicit conv : CC[T] => GenTraversableLike[T, CC[T]]) = new SizedConv[T, CC[T]](cc)
   
-  implicit def stringSizedOps(s : String) = new SizedOps[Char, String](s)
-  
-  implicit def sizedToRepr[Repr](s : Sized[Repr, _]) : Repr = s.unsized
+  implicit def stringSizedOps(s : String) = new SizedConv[Char, String](s)
 }
