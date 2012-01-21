54a81c69ef3d581976264171cf58ffc766f341b7
Miles Sabin <miles@milessabin.com>
2012-01-21 10:19:08 +0000

    Improved examples.

--- examples/src/main/scala/shapeless/examples/sized.scala
+++ examples/src/main/scala/shapeless/examples/sized.scala
@@ -1,11 +1,11 @@
 package shapeless.examples
 
-object SizedExample1 extends App {
+object SizedExamples extends App {
   import shapeless.Nat
   import shapeless.Nat._
   import shapeless.Sized
   import shapeless.Sized._
-  
+
   def sequence[T](lo : List[Option[T]]) = if (lo.exists(_ isEmpty)) None else Some(lo.map(_.get))
   
   def row(cols : Seq[String]) = cols.mkString("\"", "\", \"", "\"")
@@ -13,49 +13,56 @@ object SizedExample1 extends App {
   def csv[N <: Nat](hdrs : Sized[Seq[String], N], rows : List[Sized[Seq[String], N]]) =
     row(hdrs) :: rows.map(row(_))
   
-  val hdrs = List("Title", "Author")
-  val rows = List(List("foo", "Mr X"), List("bar", "Mr Y"))
+  def fullyStatic {
+    val hdrs = Sized("Title", "Author")  // Sized[IndexedSeq[String], _2]
+    val rows = List(                     // List[Sized[IndexedSeq[String], _2]]
+      Sized("Types and Programming Languages", "Benjamin Pierce"),
+      Sized("The Implementation of Functional Programming Languages", "Simon Peyton-Jones")
+    )
   
-  val threeHdrs = List("Title", "Author", "ISBN")
-
-  for {
-    shdrs <- hdrs.sized[_2] 
-    srows <- sequence(rows map (_.sized[_2]))
-  } {
-    val formatted = csv(shdrs, srows)
+    // hdrs and rows statically known to have the name number of columns
+    val formatted = csv(hdrs, rows)
     formatted foreach println
-  }
-
-  for {
-    shdrs <- threeHdrs.sized[_2] 
-    srows <- sequence(rows map (_.sized[_2]))
-  } {
-    // Not reached
-    val formatted = csv(shdrs, srows)
-    formatted foreach println
-  }
-}
-
-object SizedExample2 extends App {
-  import shapeless.Nat
-  import shapeless.Nat._
-  import shapeless.Sized
-  import shapeless.Sized._
-
-  def row(cols : Seq[String]) = cols.mkString("\"", "\", \"", "\"")
-  
-  def csv[N <: Nat](hdrs : Sized[Seq[String], N], rows : List[Sized[Seq[String], N]]) =
-    row(hdrs) :: rows.map(row(_))
     
-  val hdrs = Sized("Title", "Author")  // Sized[List[String], _2]
-  val rows = List(                     // List[Sized[List[String], _2]]
-    Sized("foo", "Mr X"),
-    Sized("bar", "Mr Y")
-  )
+    // threeHdrs has the wrong number of columns for rows
+    val threeHdrs = Sized("Title", "Author", "ISBN")  // Sized[IndexedSeq[Int], _3]
+    //val badFormatted = csv(threeHdrs, rows)         // Does not compile
+  }
 
-  val formatted = csv(hdrs, rows)
-  formatted foreach println
+  def mixedDynamicStatic {
+    val hdrs = List("Title", "Author")
+    val rows = List(
+      List("Types and Programming Languages", "Benjamin Pierce"),
+      List("The Implementation of Functional Programming Languages", "Simon Peyton-Jones")
+    )
+    
+    val threeHdrs = List("Title", "Author", "ISBN")
   
-  val threeHdrs = Sized("Title", "Author", "ISBN")  // Sized[List[Int], _3]
-  //val badFormatted = csv(threeHdrs, rows)         // Does not compile
+    for {
+      shdrs <- hdrs.sized[_2] 
+      srows <- sequence(rows map (_.sized[_2]))
+    } {
+      // If we get here then our lists are statically know to be
+      // of the appropriate sizes
+      val formatted = csv(shdrs, srows)
+      formatted foreach println
+    }
+  
+    for {
+      shdrs <- threeHdrs.sized[_2]   // This will be empty ... 
+      srows <- sequence(rows map (_.sized[_2]))
+    } {
+      // ... hence, not reached
+      val formatted = csv(shdrs, srows)
+      formatted foreach println
+    }
+  }
+  
+  println("Fully static: ")
+  fullyStatic
+  
+  println
+  
+  println("Mixed dynamic/static")
+  mixedDynamicStatic
 }
