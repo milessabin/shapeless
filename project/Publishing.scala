0e0a2526a172ec869dcc6cdefe4ba65fc836787a
Miles Sabin <miles@milessabin.com>
2012-04-09 21:48:27 +0100

    Added licenseplate; corrected repository hostname.

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2011 Alois Cochard 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import sbt._
 import Keys._
 
@@ -18,7 +34,7 @@ object Publishing extends Sonatype(ShapelessBuild) {
 //
 // 2. Add your sonatype credentials in '~/.ivy2/.credentials' using the following format:
 // realm=Sonatype Nexus Repository Manager
-// host=nexus.scala-tools.org
+// host=oss.sonatype.org
 // user=<username>
 // password=<password>
 
