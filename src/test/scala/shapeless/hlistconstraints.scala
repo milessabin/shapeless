12b771064597f6c574d7c871b9d5069945ba1517
aloiscochard <alois.cochard@gmail.com>
2012-04-12 14:53:15 +0200

    Added HList natural transformation constraint
    - NatTRel typeclass

--- src/test/scala/shapeless/hlistconstraints.scala
+++ src/test/scala/shapeless/hlistconstraints.scala
@@ -21,6 +21,42 @@ import org.junit.Assert._
 
 class HListConstraintsTests {
   @Test
+  def testNatTRel {
+    import TypeOperators._
+    import NatTRel._
+
+    {
+      def acceptOptionToList[L1 <: HList, L2 <: HList](l1: L1)(l2: L2)(implicit e: (Option ~??> List)#λ[L1, L2]) = l2
+
+      val l1 = Option(0) :: Option("a") :: HNil
+      val l2 = List(1) :: List("b") :: HNil
+
+      acceptOptionToList(l1)(l2)
+      //acceptOptionToId(l1)(l1)  // Does not compile
+    }
+
+    {
+      def acceptOptionToId[L1 <: HList, L2 <: HList](l1: L1)(l2: L2)(implicit e: (Option ~??> Id)#λ[L1, L2]) = l2
+
+      val l1 = Option(0) :: Option("a") :: HNil
+      val l2 = 1 :: "b" :: HNil
+
+      acceptOptionToId(l1)(l2)
+      //acceptOptionToId(l1)(l1)  // Does not compile
+    }
+
+    {
+      def acceptIdToOption[L1 <: HList, L2 <: HList](l1: L1)(l2: L2)(implicit e: (Id ~??> Option)#λ[L1, L2]) = l2
+
+      val l1 = 1 :: "b" :: HNil
+      val l2 = Option(0) :: Option("a") :: HNil
+
+      acceptIdToOption(l1)(l2)
+      //acceptIdToOption(l1)(l1)  // Does not compile
+    }
+  }
+
+  @Test
   def testUnaryTCConstraint {
     import TypeOperators._
     import UnaryTCConstraint._
