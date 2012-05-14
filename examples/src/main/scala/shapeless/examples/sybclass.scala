6baa0f718d5cad0307bc725c11ceffa9a655c5a9
Miles Sabin <miles@milessabin.com>
2012-05-14 10:58:39 +0100

    Added sources and expected output.

--- examples/src/main/scala/shapeless/examples/sybclass.scala
+++ examples/src/main/scala/shapeless/examples/sybclass.scala
@@ -26,6 +26,9 @@ object SybClassExamples extends App {
   import Poly._
   import SybClass._
 
+  // Example taken from the original SYB paper: 
+  // "Scrap your boilerplate: a practical approach to generic programming", Ralf Laemmel, Simon Peyton Jones
+  //   http://research.microsoft.com/en-us/um/people/simonpj/papers/hmap/
   case class Company[D <: HList](depts : D)
   case class Dept[S <: HList](name : Name, manager : Manager, subunits : S)
   case class Employee(person : Person, salary : Salary)
@@ -103,7 +106,25 @@ object SybClassExamples extends App {
       HNil
     )
   println(beforeRaise)
+  /* Output:
+   * Company(
+   *   Dept(
+   *     Research,Employee(Person(Ralf,Amsterdam),Salary(8000.0)),
+   *       Employee(Person(Joost,Amsterdam),Salary(1000.0)) ::
+   *       Employee(Person(Marlow,Cambridge),Salary(2000.0)) :: HNil) ::
+   *   Dept(Strategy,Employee(Person(Blair,London),Salary(100000.0)),HNil) :: HNil)
+   */
 
+  // Compute a new company structure with all salaries increased by 10%
   val afterRaise = everywhere(raise)(beforeRaise)
   println(afterRaise)
+  /* Output:
+   * Company(
+   *   Dept(
+   *     Research,Employee(Person(Ralf,Amsterdam),Salary(8800.0)),
+   *       Employee(Person(Joost,Amsterdam),Salary(1100.0)) ::
+   *       Employee(Person(Marlow,Cambridge),Salary(2200.0)) :: HNil) ::
+   *   Dept(Strategy,Employee(Person(Blair,London),Salary(110000)),HNil) :: HNil)}
+   * 
+   */
 }
