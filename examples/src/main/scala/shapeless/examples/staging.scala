86007c216cc2f2298f87c2ad67e316155a8b3834
Miles Sabin <miles@milessabin.com>
2012-07-28 12:25:55 +0100

    Added staging example.

--- examples/src/main/scala/shapeless/examples/staging.scala
+++ examples/src/main/scala/shapeless/examples/staging.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 Miles Sabin 
+ * Copyright (c) 2011 Miles Sabin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ object ReflectionUtils {
   val intTypeId = Select(Ident(newTermName("scala")), newTypeName("Int"))
   val doubleTypeId = Select(Ident(newTermName("scala")), newTypeName("Double"))
   val stringTypeId = Select(Select(Ident(newTermName("java")), newTermName("lang")), newTypeName("String"))
-  
+
   def anyToTypeId(a : Any) = a match {
     case _ : Boolean => booleanTypeId
     case _ : Int => intTypeId
@@ -36,39 +36,37 @@ object ReflectionUtils {
 
   def mkExpr[T : AbsTypeTag](mirror: Mirror)(tree : Tree) : mirror.universe.Expr[T] =
     mirror.universe.Expr[T](mirror, new TreeCreator {
-      def apply[U <: Universe with Singleton](m : MirrorOf[U]) : U#Tree = 
+      def apply[U <: Universe with Singleton](m : MirrorOf[U]) : U#Tree =
         if (m eq mirror) tree.asInstanceOf[U#Tree]
         else throw new IllegalArgumentException(s"Expr defined in $mirror cannot be migrated to other mirrors.")
     })
 }
 
-trait TupleConsumer[A, B] {
-  def apply(t : (A, B))
-}
-  
-object TupleConsumer {
-  implicit def intString = new TupleConsumer[Int, String] {
-    def apply(t : (Int, String)) { println("Using [Int, String] instance:     "+t._1+t._2) }
-  }
-  implicit def booleanDouble = new TupleConsumer[Boolean, Double] {
-    def apply(t : (Boolean, Double)) { println("Using [Boolean, Double] instance: "+(if(t._1) "+" else "-")+t._2) }
-  }
-}
-
-object StagingExamples extends App {
+object StagedTypeClassExample extends App {
   import scala.reflect.runtime.universe._
   import scala.reflect.runtime.currentMirror
   import scala.tools.reflect.Eval
   import ReflectionUtils._
-  
-  def consumeTuple[A, B](t : (A, B))(implicit tc : TupleConsumer[A, B]) = tc(t)
 
-  val tuples : List[(Any, Any)] = List((23, "foo"), (true, 2.0))
+  trait TupleConsumer[A, B] {
+    def apply(t : (A, B)) : String
+  }
 
-  for(rawTuple @ (a, b) <- tuples) {
-    val tpe1 = anyToTypeId(a)
-    val tpe2 = anyToTypeId(b)
-    
+  object TupleConsumer {
+    implicit def intString = new TupleConsumer[Int, String] {
+      def apply(t : (Int, String)) = t._1+t._2
+    }
+    implicit def booleanDouble = new TupleConsumer[Boolean, Double] {
+      def apply(t : (Boolean, Double)) = (if(t._1) "+" else "-")+t._2
+    }
+  }
+
+  def consumeTuple[A, B](t : (A, B))(implicit tc : TupleConsumer[A, B]) : String = tc(t)
+
+  def stagedConsumeTuple(rawTuple : (Any, Any)) : String = {
+    val tpe1 = anyToTypeId(rawTuple._1)
+    val tpe2 = anyToTypeId(rawTuple._2)
+
     val tupleTree =
       TypeApply(
         Select(
@@ -80,7 +78,7 @@ object StagingExamples extends App {
               Ident(newTermName("scala")),
               newTypeName("Tuple2")),
             List(tpe1, tpe2))))
-            
+
     val consumeTree =
       Apply(
         Select(
@@ -88,15 +86,28 @@ object StagingExamples extends App {
             Select(
               Ident(newTermName("shapeless")),
               newTermName("examples")),
-            newTermName("StagingExamples")),
+            newTermName("StagedTypeClassExample")),
           newTermName("consumeTuple")),
         List(tupleTree))
-    
-    val consumeExpr = mkExpr[Unit](currentMirror)(consumeTree)
-        
+
+    val consumeExpr = mkExpr[String](currentMirror)(consumeTree)
+
     val fnExpr = reify { (rawTuple : (Any, Any)) => consumeExpr.splice }
 
     val fn = fnExpr.eval
     fn(rawTuple)
   }
+
+  val t1 : (Any, Any) = (23, "foo") // Specific element types erased
+  val t2 : (Any, Any) = (true, 2.0) // Specific element types erased
+
+  // Type class instances selected on static type at runtime!
+
+  val c1 = stagedConsumeTuple(t1) // Uses intString instance
+  assert(c1 == "23foo")
+  println(c1)
+
+  val c2 = stagedConsumeTuple(t2) // Uses booleanDouble instance
+  assert(c2 == "+2.0")
+  println(c2)
 }
