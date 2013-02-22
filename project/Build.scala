459ee09bf813c13abe364b166d92cdf290d75e0e
Miles Sabin <miles@milessabin.com>
2013-02-21 19:32:43 -0700

    Bumped version to 1.2.5-SNAPSHOT.

--- project/Build.scala
+++ project/Build.scala
@@ -86,7 +86,7 @@ object ShapelessBuild extends Build {
   def commonSettings = Defaults.defaultSettings ++
     Seq(
       organization        := "com.chuusai",
-      version             := "1.2.4-SNAPSHOT",
+      version             := "1.2.5-SNAPSHOT",
       scalaVersion        := "2.11.0-SNAPSHOT",
 
       (unmanagedSourceDirectories in Compile) <<= (scalaSource in Compile)(Seq(_)),
