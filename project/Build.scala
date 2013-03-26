e7635b9c692abfde1fdb6a1f91441a96e25889ad
Miles Sabin <miles@milessabin.com>
2013-03-26 10:47:45 +0000

    Drop support for Scala < 2.9.2.

--- project/Build.scala
+++ project/Build.scala
@@ -86,7 +86,7 @@ object ShapelessBuild extends Build {
       organization        := "com.chuusai",
       version             := "1.2.5-SNAPSHOT",
       scalaVersion        := "2.9.3",
-      crossScalaVersions  := Seq("2.9.1", "2.9.1-1", "2.9.2", "2.9.3"),
+      crossScalaVersions  := Seq("2.9.2", "2.9.3"),
       crossVersion        := CrossVersion.full,
       scalaBinaryVersion <<= scalaVersion,
 
