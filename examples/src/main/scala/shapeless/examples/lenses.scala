ed6797d0f39e20f6ade2bf69fd4282e86229fbec
jto <boudhevil@gmail.com>
2014-04-07 14:45:42 +0200

    Examples for Lens with labels as indices

--- examples/src/main/scala/shapeless/examples/lenses.scala
+++ examples/src/main/scala/shapeless/examples/lenses.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-14 Miles Sabin 
+ * Copyright (c) 2012-14 Miles Sabin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,31 +18,40 @@ package shapeless.examples
 
 /*
  * Examples of Lens usage.
- * 
+ *
  * @author Miles Sabin
  */
-object LenseExamples extends App {
-  import shapeless._
-  import test._
-  import Lens._
 
+package classes {
   // A pair of ordinary case classes ...
   case class Address(street : String, city : String, postcode : String)
   case class Person(name : String, age : Int, address : Address)
-  
+}
+
+import shapeless._
+import Lens._
+import classes._
+
+trait Lenses {
   // Some lenses over Person/Address ...
-  val nameLens     = Lens[Person] >> 0
-  val ageLens      = Lens[Person] >> 1
-  val addressLens  = Lens[Person] >> 2
-  val streetLens   = Lens[Person] >> 2 >> 0
-  val cityLens     = Lens[Person] >> 2 >> 1
-  val postcodeLens = Lens[Person] >> 2 >> 2
+  val nameLens: Lens[Person, String]
+  val ageLens: Lens[Person, Int]
+  val addressLens: Lens[Person, Address]
+  val streetLens: Lens[Person, String]
+  val cityLens: Lens[Person, String]
+  val postcodeLens: Lens[Person, String]
+}
+
+trait LenseExamples {
+  self: Lenses =>
+
+  import test._
 
   // Starting value
   val person = Person("Joe Grey", 37, Address("Southover Street", "Brighton", "BN2 9UA"))
-  
+
   // Atomic lenses ...
-  
+
   // Read a field
   val age1 = ageLens.get(person) // Type inferred is Int
   typed[Int](age1)
@@ -51,35 +60,58 @@ object LenseExamples extends App {
   // Update a field
   val person2 = ageLens.set(person)(38)
   assert(person2.age == 38)
-  
+
   // Transform a field
   val person3 = ageLens.modify(person2)(_ + 1)
   assert(person3.age == 39)
-  
+
   // Read a nested field
   val street = streetLens.get(person3)
   assert(street == "Southover Street")
-  
+
   // Update a nested field
   val person4 = streetLens.set(person3)("Montpelier Road")
   assert(person4.address.street == "Montpelier Road")
-  
+
   // Cumulative result of above updates
   assert(person4 == Person("Joe Grey", 39, Address("Montpelier Road", "Brighton", "BN2 9UA")))
   println(person4)
-  
+
   // Product/composite lenses ...
-  
+
   // Create a product lens spanning Person and Address
   val nameAgeCityLens = nameLens ~ ageLens ~ cityLens
-  
-  val nac1 = nameAgeCityLens.get(person) // Inferred type is the expected tuple type 
+
+  val nac1 = nameAgeCityLens.get(person) // Inferred type is the expected tuple type
   typed[(String, Int, String)](nac1)
   assert(nac1 == ("Joe Grey", 37, "Brighton"))
   println(nac1)
-  
+
   // Update with a tuple distributing values across Person and Address
   val person5 = nameAgeCityLens.set(person)("Joe Soap", 27, "London")
   assert(person5 == Person("Joe Soap", 27, Address("Southover Street", "London", "BN2 9UA")))
   println(person5)
 }
+
+
+trait NatLenses extends Lenses {
+  val nameLens     = Lens[Person] >> 0
+  val ageLens      = Lens[Person] >> 1
+  val addressLens  = Lens[Person] >> 2
+  val streetLens   = Lens[Person] >> 2 >> 0
+  val cityLens     = Lens[Person] >> 2 >> 1
+  val postcodeLens = Lens[Person] >> 2 >> 2
+}
+
+object NatLensesExamples extends NatLenses with LenseExamples with App
+
+trait LabbeledLenses extends Lenses {
+  val nameLens     = Lens[Person] >> 'name
+  val ageLens      = Lens[Person] >> 'age
+  val addressLens  = Lens[Person] >> 'address
+  val streetLens   = Lens[Person] >> 'address >> 'street
+  val cityLens     = Lens[Person] >> 'address >> 'city
+  val postcodeLens = Lens[Person] >> 'address >> 'postcode
+}
+
+object LabelleddLensesExamples extends LabbeledLenses with LenseExamples with App
