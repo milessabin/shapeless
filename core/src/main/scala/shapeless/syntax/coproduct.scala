e86e03346be7d24c252c327f6ee87ffa9531a73c
Stacy Curl <stacy.curl@gmail.com>
2014-07-12 21:21:19 +0100

    Rename Coproduct append/Append to extendRight/ExtendRight

--- core/src/main/scala/shapeless/syntax/coproduct.scala
+++ core/src/main/scala/shapeless/syntax/coproduct.scala
@@ -38,7 +38,7 @@ final class CoproductOps[C <: Coproduct](c: C) {
 
   def length(implicit length: Length[C]): length.Out = length()
 
-  def append[T](implicit append: Append[C, T]): append.Out = append(c)
+  def extendRight[T](implicit extendRight: ExtendRight[C, T]): extendRight.Out = extendRight(c)
 
   def rotateLeft[N <: Nat](implicit rotateLeft: RotateLeft[C, N]): rotateLeft.Out = rotateLeft(c)
   def rotateLeft[N <: Nat](n: N)(implicit rotateLeft: RotateLeft[C, n.N]): rotateLeft.Out = rotateLeft(c)
--- core/src/test/scala/shapeless/coproduct.scala
+++ core/src/test/scala/shapeless/coproduct.scala
@@ -255,16 +255,16 @@ class CoproductTests {
   }
 
   @Test
-  def testAppend {
+  def testExtendRight {
     type S = String; type I = Int; type D = Double; type C = Char
     type CoI    = I :+: CNil
     type CoIS   = I :+: S :+: CNil
     type CoISD  = I :+: S :+: D :+: CNil
     type CoISDC = I :+: S :+: D :+: C :+: CNil
 
-    assertTypedEquals[CoIS](Coproduct[CoIS](1), Coproduct[CoI](1).append[S])
-    assertTypedEquals[CoISD](Coproduct[CoISD](1), Coproduct[CoIS](1).append[D])
-    assertTypedEquals[CoISDC](Coproduct[CoISDC](1), Coproduct[CoISD](1).append[C])
+    assertTypedEquals[CoIS](Coproduct[CoIS](1), Coproduct[CoI](1).extendRight[S])
+    assertTypedEquals[CoISD](Coproduct[CoISD](1), Coproduct[CoIS](1).extendRight[D])
+    assertTypedEquals[CoISDC](Coproduct[CoISDC](1), Coproduct[CoISD](1).extendRight[C])
   }
 
   @Test
