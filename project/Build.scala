78426a0ae9f688189b66984aee5d5e5039ee32c5
Miles Sabin <miles@milessabin.com>
2014-07-24 07:26:51 +0100

    Bumped Scala version to 2.11.2.

--- project/Build.scala
+++ project/Build.scala
@@ -180,7 +180,7 @@ object ShapelessBuild extends Build {
   def commonSettings =
     Seq(
       organization        := "com.chuusai",
-      scalaVersion        := "2.11.1",
+      scalaVersion        := "2.11.2",
 
       (unmanagedSourceDirectories in Compile) <<= (scalaSource in Compile)(Seq(_)),
       (unmanagedSourceDirectories in Test) <<= (scalaSource in Test)(Seq(_)),
