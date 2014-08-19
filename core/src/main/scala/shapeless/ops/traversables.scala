7b997c828c4cfb74b7c75291c888ff3f3ae29bd2
Stacy Curl <stacy.curl@gmail.com>
2014-08-19 08:51:08 +0100

    Added Traversable[HList].toNestedMap

--- core/src/main/scala/shapeless/ops/traversables.scala
+++ core/src/main/scala/shapeless/ops/traversables.scala
@@ -51,4 +51,36 @@ object traversable {
           else for(h <- l.head.cast[OutH]; t <- flt(l.tail)) yield h :: t
     }
   }
+
+  /**
+   * Type class witnessing that a Traversable of HLists can be converted into a nested map,
+   * the depth of the nesting depending on the size of each HList.
+   *
+   * @author Stacy Curl
+   */
+  trait ToNestedMap[L <: HList] extends DepFn1[Traversable[L]]
+
+  object ToNestedMap {
+    def apply[L <: HList]
+      (implicit toNestedMap: ToNestedMap[L]): Aux[L, toNestedMap.Out] = toNestedMap
+
+    type Aux[L <: HList, Out0] = ToNestedMap[L] { type Out = Out0 }
+
+    implicit def nonNestedMap[A, B]: Aux[A :: B :: HNil, A Map B] = new ToNestedMap[A :: B :: HNil] {
+      type Out = A Map B
+
+      def apply(l: Traversable[A :: B :: HNil]): A Map B =
+        l.map(ab => (ab.head, ab.tail.head))(collection.breakOut)
+    }
+
+    implicit def nestedMap[H, T <: HList, TOut](
+      implicit nestedMap: Aux[T, TOut]
+    ): Aux[H :: T, H Map TOut] = new ToNestedMap[H :: T] {
+      type Out = H Map TOut
+
+      def apply(l: Traversable[H :: T]): H Map TOut =
+        l.groupBy(_.head).mapValues(values => nestedMap(values.map(_.tail)))
+    }
+  }
+
 }
