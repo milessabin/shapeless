20821f10a3db8a081ee3fabdde4a24a50b563735
Mario Pastorelli <mario.pastorelli@teralytics.ch>
2015-01-05 23:45:39 +0100

    Add CSV serialization/deserialization example

---
+++ examples/src/main/scala/shapeless/examples/csv.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2014 Mario Pastorelli (pastorelli.mario@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package shapeless.examples
+
+import shapeless._, syntax.singleton._
+
+import scala.util.{Try,Success,Failure}
+
+
+/*
+ * This example shows how to createa a serializer/deserializer from CSV to
+ * products.
+ * */
+
+// The class to serialize or deserialize
+case class Person(name: String, surname: String, age: Int)
+
+object CSVExample extends App {
+
+  import CSVConverter._
+
+  val hListConverter = CSVConverter[String :: String :: Int :: HNil]
+
+  println(hListConverter.from("mario,pastorelli,28"))
+}
+
+
+// Implementation
+
+/** Exception to throw if something goes wrong during CSV parsing */
+class CSVException(s: String) extends RuntimeException
+
+/** Trait for types that can be serialized to/deserielized from CSV */
+trait CSVConverter[T] {
+  def from(s: String): Try[T]
+  def to(t: T): String
+}
+
+/** Isntances of the CSVConverter trait */
+object CSVConverter {
+  def apply[T](implicit st: Lazy[CSVConverter[T]]): CSVConverter[T] = st.value
+
+  def fail(s: String) = Failure(new CSVException(s))
+
+
+  // Primitives
+
+  implicit def stringCSVConverter: CSVConverter[String] = new CSVConverter[String] {
+    def from(s: String): Try[String] = Success(s)
+    def to(s: String): String = s
+  }
+
+  implicit def intCsvConverter: CSVConverter[Int] = new CSVConverter[Int] {
+    def from(s: String): Try[Int] = Try(s.toInt)
+    def to(i: Int): String = i.toString
+  }
+
+
+  // HList
+
+  implicit def deriveHNil: CSVConverter[HNil] =
+    new CSVConverter[HNil] {
+      def from(s: String): Try[HNil] = s match {
+        case "" => Success(HNil)
+        case s => fail("Cannot convert '" ++ s ++ "' to HNil")
+      }
+      def to(n: HNil) = ""
+    }
+  
+  implicit def deriveHCons[V, T <: HList]
+    (implicit scv: Lazy[CSVConverter[V]], sct: Lazy[CSVConverter[T]])
+        : CSVConverter[V :: T] =
+      new CSVConverter[V :: T] {
+
+        def from(s: String): Try[V :: T] = s.span(_ != ',') match {
+          case (before,after) =>
+            for {
+              front <- scv.value.from(before)
+              back <- sct.value.from(if (after.isEmpty) after else after.tail)
+            } yield front :: back
+
+          case _ => fail("Cannot convert '" ++ s ++ "' to HList")
+        }
+
+        def to(ft: V :: T): String = {
+          scv.value.to(ft.head) ++ "," ++ sct.value.to(ft.tail)
+        }
+      }
+}
