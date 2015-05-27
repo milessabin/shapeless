4401bdd4f68f77c6709306495e5c42d7d1e46c22
Miles Sabin <miles@milessabin.com>
2015-05-27 12:14:03 +0100

    Added merge-all script.

---
+++ merge-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+
+if [ -n "$(git status --porcelain)" ]; then 
+  echo "There are uncommitted changes ... aborting"
+else 
+  git checkout scala-2.10.x
+  git merge master
+  git checkout scalajs-2.11.x
+  git merge master
+  git checkout scalajs-2.10.x
+  git merge scala-2.10.x
+  git checkout master
+fi
