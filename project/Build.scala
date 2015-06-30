212956fc4362e63c1bf3dd76a4b73aa364ef083d
Miles Sabin <miles@milessabin.com>
2015-06-30 15:05:10 +0100

    No Scala.js for 2.11.7 yet.

--- project/Build.scala
+++ project/Build.scala
@@ -165,7 +165,7 @@ object ShapelessBuild extends Build {
   def commonSettings =
     Seq(
       organization        := "com.chuusai",
-      scalaVersion        := "2.11.7",
+      scalaVersion        := "2.11.6",
 
       (unmanagedSourceDirectories in Compile) <<= (scalaSource in Compile)(Seq(_)),
       (unmanagedSourceDirectories in Test) <<= (scalaSource in Test)(Seq(_)),
