0021e6067301eb6ad1714122dddc7c666fbef26d
Miles Sabin <miles@milessabin.com>
2016-01-26 16:33:28 +0000

    Redundant superclass in a test isn't a problem; added licenseplate.

--- core/src/test/scala/shapeless/annotation.scala
+++ core/src/test/scala/shapeless/annotation.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2015-6 Alexandre Archambault
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package shapeless
 
 import scala.annotation.{ Annotation => saAnnotation }
@@ -5,8 +21,6 @@ import org.junit.Test
 import shapeless.test.illTyped
 
 object AnnotationTestsDefinitions {
-
-  // BACKPORT: No need to extend scala.annotation.Annotation in 2.11+
 
   case class First() extends saAnnotation
   case class Second(i: Int, s: String) extends saAnnotation
