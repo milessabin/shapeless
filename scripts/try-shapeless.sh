a27582892817890df772e95fd9b3bac00ff15c3b
Miles Sabin <miles@milessabin.com>
2016-08-18 14:03:14 +0100

    Bumped shapeless version to 2.3.2.

--- scripts/try-shapeless.sh
+++ scripts/try-shapeless.sh
@@ -3,7 +3,7 @@ COURSIER_URL=https://raw.githubusercontent.com/alexarchambault/coursier/v1.0.0-M
 test -e ~/.coursier/cr || (mkdir -p ~/.coursier && curl -s --output ~/.coursier/cr $COURSIER_URL && chmod +x ~/.coursier/cr)
 CLASSPATH="$(~/.coursier/cr fetch -q -p \
   \
-  com.chuusai:shapeless_2.11:2.3.1 \
+  com.chuusai:shapeless_2.11:2.3.2 \
   com.lihaoyi:ammonite-repl_2.11.8:0.6.2 \
   \
 )" java ammonite.repl.Main --predef 'import shapeless._'
