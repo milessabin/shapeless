f81b1a9209378f46383506b210a2b281d84ade99
n4to4 <n4to4k@gmail.com>
2017-02-24 10:48:20 +0900

    Update Coursier and Ammonite versions for try-shapeless.sh

--- scripts/try-shapeless.sh
+++ scripts/try-shapeless.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-COURSIER_URL=https://raw.githubusercontent.com/alexarchambault/coursier/v1.0.0-M14-2/coursier
+COURSIER_URL=https://raw.githubusercontent.com/alexarchambault/coursier/v1.0.0-M15/coursier
 test -e ~/.coursier/coursier || \
   (mkdir -p ~/.coursier && curl -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
 ~/.coursier/coursier launch -q -P \
-  com.lihaoyi:ammonite_2.11.8:0.7.8 \
+  com.lihaoyi:ammonite_2.11.8:0.8.2 \
   com.chuusai:shapeless_2.11:2.3.2 \
   -- --predef 'import shapeless._' < /dev/tty
