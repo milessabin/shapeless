742604e1cf7ecfdbd0de743169a0af373052ccc8
Zainab <zainab.ali.london@gmail.com>
2017-05-22 22:55:11 +0100

    Add Refute typeclass

---
+++ core/src/main/scala/shapeless/refute.scala
@@ -0,0 +1,22 @@
+package shapeless
+
+import scala.annotation.implicitNotFound
+
+/** Evidence that no implicit instance of type `T` is available
+  *
+  *  @author Zainab Ali
+  */
+@implicitNotFound("Evidence $T was found")
+trait Refute[T]
+
+object Refute {
+
+  /** This results in  ambigous implicits if there is implicit evidence of `T` */
+  implicit def ambiguousIfPresent[T](implicit ev: T): Refute[T] = new Refute[T] {}
+
+  /** This always declares an instance of `Refute`
+    *
+    * This instance will only be found when there is no evidence of `T`
+    * */
+  implicit def refute[T]: Refute[T] = new Refute[T] {}
+}
---
+++ core/src/test/scala/shapeless/refute.scala
@@ -0,0 +1,36 @@
+package shapeless
+
+import org.junit.Test
+
+import test._
+
+object RefuteTests {
+  class PresentEvidence
+  implicit val presentEvidence: PresentEvidence = new PresentEvidence
+
+  class NotPresentEvidence
+
+  class InductiveEvidence
+  implicit def inductiveEvidence(implicit ev: Refute[NotPresentEvidence]): InductiveEvidence = new InductiveEvidence
+}
+
+class RefuteTests {
+
+  import RefuteTests._
+
+  @Test
+  def testRefuteNotFound {
+    illTyped("the[Refute[PresentEvidence]]")
+  }
+
+  @Test
+  def testRefuteNotPresent {
+    the[Refute[NotPresentEvidence]]
+  }
+
+  @Test
+  def testInductivePresent {
+    the[InductiveEvidence]
+  }
+
+}
