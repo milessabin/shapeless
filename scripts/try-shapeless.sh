466f5c37e502b2d24bd0c15a2f07b60b92f6dae0
Miles Sabin <miles@milessabin.com>
2017-07-30 11:18:53 +0100

    Bumped Ammonite and Scala version.

--- scripts/try-shapeless.sh
+++ scripts/try-shapeless.sh
@@ -3,6 +3,6 @@ COURSIER_URL=https://git.io/vgvpD
 test -e ~/.coursier/coursier || \
   (mkdir -p ~/.coursier && curl -L -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
 ~/.coursier/coursier launch -q -P \
-  com.lihaoyi:ammonite_2.12.2:1.0.0 \
+  com.lihaoyi:ammonite_2.12.3:1.0.1 \
   com.chuusai:shapeless_2.12:2.3.2 \
   -- --predef-code 'import shapeless._' < /dev/tty
