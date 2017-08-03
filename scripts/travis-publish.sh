fb0773d26ddd76ba45690043528a2d3e908e8a57
fabio labella <fabio.labella@ncr.com>
2017-08-03 16:24:15 +0100

    sbt 1.1.1

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -3,7 +3,7 @@
 # Example setting to use at command line for testing:
 # export TRAVIS_SCALA_VERSION=2.10.5;export TRAVIS_PULL_REQUEST="false";export TRAVIS_BRANCH="master"
 
-SBT="sbt ++${TRAVIS_SCALA_VERSION}"
+SBT="sbt ++${TRAVIS_SCALA_VERSION}!"
 COVERAGE="$SBT clean coverage test coverageReport"
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" &&
