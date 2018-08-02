dfa759f7b42094dc4c8010c456f082d56df1f3b8
afsaltthaj <afsal.taj06@gmail.com>
2018-08-02 22:46:01 +1000

    Add fix for lazy tagged type

--- core/src/test/scala/shapeless/labelledgeneric.scala
+++ core/src/test/scala/shapeless/labelledgeneric.scala
@@ -93,7 +93,7 @@ object ScalazTaggedAux {
     def apply(): String
   }
 
-  object TC {
+  object TC extends TCLowPriority {
     implicit val intTC: TC[Int] =
       new TC[Int] {
         def apply() = "Int"
@@ -114,22 +114,13 @@ object ScalazTaggedAux {
         def apply() = "HNil"
       }
 
-    implicit def hconsTCTagged[K <: Symbol, H, HT, T <: HList](implicit
-      key: Witness.Aux[K],
-      headTC: Lazy[TC[H @@ HT]],
-      tailTC: Lazy[TC[T]]
-    ): TC[FieldType[K, H @@ HT] :: T] =
-      new TC[FieldType[K, H @@ HT] :: T] {
-        def apply() = s"${key.value.name}: ${headTC.value()} :: ${tailTC.value()}"
-      }
-
     implicit def hconsTC[K <: Symbol, H, T <: HList](implicit
       key: Witness.Aux[K],
       headTC: Lazy[TC[H]],
-      tailTC: Lazy[TC[T]]
+      tailTC: TC[T]
     ): TC[FieldType[K, H] :: T] =
       new TC[FieldType[K, H] :: T] {
-        def apply() = s"${key.value.name}: ${headTC.value()} :: ${tailTC.value()}"
+        def apply() = s"${key.value.name}: ${headTC.value()} :: ${tailTC}"
       }
 
     implicit def projectTC[F, G](implicit
@@ -138,6 +129,18 @@ object ScalazTaggedAux {
     ): TC[F] =
       new TC[F] {
         def apply() = s"Proj(${tc.value()})"
+      }
+  }
+
+  abstract class TCLowPriority {
+    // FIXME: Workaround #309
+    implicit def hconsTCTagged[K <: Symbol, H, HT, T <: HList](implicit
+                                                               key: Witness.Aux[K],
+                                                               headTC: Lazy[TC[H @@ HT]],
+                                                               tailTC: TC[T]
+                                                              ): TC[FieldType[K, H @@ HT] :: T] =
+      new TC[FieldType[K, H @@ HT] :: T] {
+        def apply() = s"${key.value.name}: ${headTC.value()} :: ${tailTC()}"
       }
   }
 }
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-version in ThisBuild := "2.4.0-SNAPSHOT"
+version in ThisBuild := "2.4.0-FIXTAG"
