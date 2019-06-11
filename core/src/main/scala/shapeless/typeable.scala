0acb3923bf6379fa53f87d394a487f0345c33d89
Jeremy Smith <jeremys@netflix.com>
2019-06-11 11:30:30 +0200

    Loosen typeable restrictions
    Fixes #812
    
    Don't reject materializing a `Typeable` instance due to the presence of
    a non-case-accessor value, unless its type is abstract.

--- core/src/main/scala/shapeless/typeable.scala
+++ core/src/main/scala/shapeless/typeable.scala
@@ -464,9 +464,19 @@ class TypeableMacros(val c: blackbox.Context) extends SingletonTypeUtils {
   }
 
   private def mkCaseClassTypeable(tpe: Type): Tree = {
+    // an unsafe accessor is one that isn't a case class accessor but has an abstract type.
+    def isUnsafeAccessor(sym: TermSymbol): Boolean = {
+      if (sym.isCaseAccessor) {
+        false
+      } else if (sym.typeSignature.typeSymbol.isAbstract) {
+        sym.isVal ||
+        sym.isVar ||
+        (sym.isParamAccessor && !(sym.accessed.isTerm && sym.accessed.asTerm.isCaseAccessor))
+      } else false
+    }
+
     val nonCaseAccessor = tpe.decls.exists {
-      case sym: TermSymbol if !sym.isCaseAccessor && (sym.isVal || sym.isVar ||
-        (sym.isParamAccessor && !(sym.accessed.isTerm && sym.accessed.asTerm.isCaseAccessor))) => true
+      case sym: TermSymbol if isUnsafeAccessor(sym) => true
       case _ => false
     }
     if (nonCaseAccessor) {
