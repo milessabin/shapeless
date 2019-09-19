a58dbfdb28fc2616d0d9ff95617f88f5fc575142
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-09-19 13:10:02 +0200

    Use Expr.ofTuple

--- core/src/main/scala/shapeless/annotation.scala
+++ core/src/main/scala/shapeless/annotation.scala
@@ -155,7 +155,7 @@ object AnnotationMacros {
       import r._
 
       def mkAnnotations(annotTrees: Seq[Expr[Any]]): Expr[Annotations[A, T]] =
-        annotTrees.toExprOfTuple match {
+        Expr.ofTuple(annotTrees) match {
           case '{ $t: $tt } => '{ Annotations.mkAnnotations[A, T, $tt & Tuple]($t) }
         }
 
