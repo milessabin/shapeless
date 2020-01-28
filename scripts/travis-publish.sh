ed1f9ae112858c63d4f5ff21929b93bbbab125f7
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-01-28 17:16:19 -0300

    Split coreJVM/coreJS test and publication
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -11,9 +11,11 @@ if [[ "${TRAVIS_PULL_REQUEST}" == "false" &&
       "${TRAVIS_REPO_SLUG}" == "milessabin/shapeless" &&
       $(cat version.sbt) =~ "-SNAPSHOT"
 ]]; then
-PUBLISH=publish
+PUBLISH=coreJVM/publish
+PUBLISHJS=coreJS/publish
 else
-PUBLISH=publishLocal
+PUBLISH=coreJVM/publishLocal
+PUBLISHJS=coreJS/publishLocal
 fi
 
 if [[ "$NATIVE" == "true" ]]; then
@@ -21,5 +23,8 @@ if [[ "$NATIVE" == "true" ]]; then
 #elif [[ "$TRAVIS_SCALA_VERSION" == 2.13.* ]]; then
 #    eval $COVERAGE && ${SBT} validate ${PUBLISH} && codecov
 else
-    ${SBT} clean validate ${PUBLISH}
+  ${SBT} clean validateJVM ${PUBLISH}
+  for sjs in '1.0.0-RC2' '0.6.32'; do
+    SCALAJS_VERSION=$sjs ${SBT} clean validateJS ${PUBLISHJS}
+  done
 fi
