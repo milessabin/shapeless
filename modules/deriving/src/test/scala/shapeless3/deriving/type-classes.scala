c4123ca720f1fdae701c5e827184d5d5a77f5aeb
Tim Spence <timothywspence@gmail.com>
2021-05-04 11:33:28 +0100

    Derive foldable

--- modules/deriving/src/test/scala/shapeless3/deriving/type-classes.scala
+++ modules/deriving/src/test/scala/shapeless3/deriving/type-classes.scala
@@ -279,17 +279,43 @@ object Eval {
 trait Foldable[F[_]] {
   def foldLeft[A, B](fa: F[A])(b: B)(f: (B, A) => B): B
 
-  def foldRight[A, B](fa: F[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B]
+  def foldRight[A, B](fa: F[A])(lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B]
 
   given Foldable[Id] with
     def foldLeft[A, B](fa: Id[A])(b: B)(f: (B, A) => B): B = f(b, fa)
 
-    def foldRight[A, B](fa: Id[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = f(fa, lb)
+    def foldRight[A, B](fa: Id[A])(lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = f(fa, lb)
 
   given [X]: Foldable[Const[X]] with
     def foldLeft[A, B](fa: Const[X][A])(b: B)(f: (B, A) => B): B = b
 
-    def foldRight[A, B](fa: Const[X][A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = lb
+    def foldRight[A, B](fa: Const[X][A])(lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = lb
+
+  given foldableProduct[F[_]](using inst: => K1.ProductInstances[Foldable, F]): Foldable[F] with
+    def foldLeft[A, B](fa: F[A])(b: B)(f: (B, A) => B): B =
+      inst.foldLeft[A, B](fa)(b)(
+        [t[_]] => (acc: B, fd: Foldable[t], t0: t[A]) => Continue(fd.foldLeft(t0)(acc)(f))
+      )
+
+    def foldRight[A, B](fa: F[A])(lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
+      inst.foldRight[A, Eval[B]](fa)(lb)(
+        [t[_]] => (fd: Foldable[t], t0: t[A], acc: Eval[B]) => Continue(fd.foldRight(t0)(acc)(f))
+      )
+
+  given foldableCoproduct[F[_]](using inst: => K1.CoproductInstances[Foldable, F]): Foldable[F] with
+    def foldLeft[A, B](fa: F[A])(b: B)(f: (B, A) => B): B =
+      inst.fold[A, B](fa)(
+        [t[_]] => (fd: Foldable[t], t0: t[A]) => fd.foldLeft(t0)(b)(f)
+      )
+
+    def foldRight[A, B](fa: F[A])(lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
+      inst.fold[A, Eval[B]](fa)(
+        [t[_]] => (fd: Foldable[t], t0: t[A]) => fd.foldRight(t0)(lb)(f)
+      )
+
+  inline def derived[F[_]](using gen: => K1.Generic[F]): Foldable[F] =
+    gen.derive(foldableProduct, foldableCoproduct)
+
 }
 
 object Foldable {
