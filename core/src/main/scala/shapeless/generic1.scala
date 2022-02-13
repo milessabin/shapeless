031a92538f38c5e90a79d9323c08699ed03f7d28
Georgi Krastev <joro.kr.21@gmail.com>
2022-02-13 11:58:43 +0100

    Replace CtorDtor with simplified FromTo
    `FromTo` decouples and simplifies the strategy to generate the
    `from` and `to` functions. This will let us more easily add
    other strategies in the future and refine the choice of which
    strategy to use in which case.

--- core/src/main/scala/shapeless/generic1.scala
+++ core/src/main/scala/shapeless/generic1.scala
@@ -217,17 +217,12 @@ class Generic1Macros(val c: whitebox.Context) extends CaseClassMacros {
   }
 
   def mkProductGeneric1(tpe: Type, frTpe: Type): Tree = {
-    val ctorDtor = CtorDtor(tpe)
-    val (p, ts) = ctorDtor.binding
-    val to = cq"$p => ${mkHListValue(ts)}"
-    val (rp, rts) = ctorDtor.reprBinding
-    val from = cq"$rp => ${ctorDtor.construct(rts)}"
-    val name = TypeName(c.freshName("P"))
-    val reprTpt = reprTypTree1(tpe, name)
-    val reprName = TypeName(c.freshName("R"))
-
+    val tparam = c.freshName(TypeName("P"))
+    val reprName = c.freshName(TypeName("R"))
+    val reprTpt = reprTypTree1(tpe, tparam)
+    val FromTo(from, to) = FromTo.of(tpe, tq"$reprName[$AnyTpe]")
     q"""
-      type $reprName[$name] = $reprTpt
+      type $reprName[$tparam] = $reprTpt
       $generic1.unsafeInstance[$tpe, $frTpe, $reprName]({ case $to }, { case $from })
     """
   }
